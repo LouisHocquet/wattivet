14d272cb54f7b8b5ffb7ec7755281895226449c3
Louis Hocquet <louis.hocquet@sfr.fr>
2024-03-25 22:22:53 +0100

    Revert "Added redirection root to www subdomain"
    This reverts commit a393b9c815881275ae0b218b44d3c1f3226b6e19.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :redirect_root_domain
-
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
-  end
-
-  private
-
-  def redirect_root_domain
-    return unless request.host == 'wattivet.fr'
-
-    redirect_to("#{request.protocol}www.wattivet.fr#{request.fullpath}", status: 301)
   end
 end
